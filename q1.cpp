cd36386770d66375abec44c0c98c105bb64e51e8
beatrizsouto3 <absouto.silva@gmail.com>
2025-03-31 11:59:07 -0300

    new

--- q1.cpp
+++ q1.cpp
@@ -7,5 +7,5 @@ int main () {
   cout <<"\n\tDigite sua ideade em anos, meses e dias: ";
   cin >> A >> M >> D;
   Dias_Totais = (A*365)+(M*30)+D;
-  cout <<"\n\tDias totais: " << Dias_Totais <<endl;
+  cout <<"\n\tDias totais: " << Dias_Totais << " dias." <<endl;
 }
---
+++ q7.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main (){
+  float C, F;
+  cout << "\n\tDigite a temperatura em graus Celsius (C): ";
+  cin >> C;
+  F = (9*C+160)/5;
+  cout <<"\n\tTemperatura convertida em Fahrenheit (F): " << F << "F." << endl;
+}
