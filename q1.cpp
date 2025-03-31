2eac8f49681651c470e2d487bbc9d1e9ea535f84
beatrizsouto3 <absouto.silva@gmail.com>
2025-03-30 22:51:53 -0300

    codigos

---
+++ q1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main () {
+  int A, M, D, Dias_Totais;
+  cout <<"\n\tDigite sua ideade em anos, meses e dias: ";
+  cin >> A >> M >> D;
+  Dias_Totais = (A*365)+(M*30)+D;
+  cout <<"\n\tDias totais: " << Dias_Totais <<endl;
+}
