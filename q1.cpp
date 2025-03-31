c5f6e641997c25235f3d0b8f45e333248a9520c7
beatrizsouto3 <absouto.silva@gmail.com>
2025-03-31 12:01:05 -0300

    new

--- q1.cpp
+++ q1.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main () {
   int A, M, D, Dias_Totais;
-  cout <<"\n\tDigite sua ideade em anos, meses e dias: ";
+  cout <<"\n\tDigite sua idade em anos, meses e dias: ";
   cin >> A >> M >> D;
   Dias_Totais = (A*365)+(M*30)+D;
   cout <<"\n\tDias totais: " << Dias_Totais << " dias." <<endl;
