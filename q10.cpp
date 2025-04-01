25e83fd40504e9c2f3dedb61c57bca2338a333a0
beatrizsouto3 <absouto.silva@gmail.com>
2025-04-01 00:14:00 -0300

    new

--- q10.cpp
+++ q10.cpp
@@ -10,7 +10,7 @@ int main() {
     cin >> A;
     cout << "\n\tDigite o valor de B: ";
     cin >> B;
-    cout << "\n\tadicao: " << A+B << endl;
+    cout << "\n\tAdicao: " << A+B << endl;
     cout << "\n\tSubtracao: " << A-B << endl;
     cout << "\n\tMultiplicacao: " << A*B << endl;
     cout << "\n\tResto da divisao: " << (int)A % (int)B << endl;
@@ -20,7 +20,7 @@ int main() {
     cin >> C;
     cout << "\n\tDigite o valor logico de D (0 para false, 1 para true): ";
     cin >> D;
-    cout << "\n\tNegaacao de C: " << !C << endl;
+    cout << "\n\tNegacao de C: " << !C << endl;
     cout << "\n\tNegacao de D: " << !D << endl;
     cout << "\n\tConjuncao (C && D): " << (C && D) << endl;
     cout << "\n\tDisjuncao (C || D): " << (C || D) << endl;
